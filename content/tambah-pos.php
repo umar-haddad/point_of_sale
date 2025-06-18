1a4fab029d0d1f86fa34820ca2134b66e4cd73b1
tiedsandi <fachransandi@gmail.com>
2025-06-18 15:08:15 +0700

    addPos

--- content/tambah-pos.php
+++ content/tambah-pos.php
@@ -42,7 +42,9 @@ $id_user = isset($_GET['add-user-role']) ? $_GET['add-user-role'] : '';
 $queryRoles = mysqli_query($config, "SELECT * FROM roles ORDER BY role_id Desc");
 $rowRoles = mysqli_fetch_all($queryRoles, MYSQLI_ASSOC);
 
-$queryUserRoles = mysqli_query($config, "SELECT user_roles.*, roles.role_name FROM user_roles
+
+//dirumah si 
+$queryUserRoles = mysqli_query($config, "SELECT user_roles.*, roles.role_name FROM user_roles 
                               LEFT JOIN roles ON user_roles.id_role = roles.role_id
                               WHERE id_user = '$id_user'
                               ORDER BY user_roles.uR_id DESC");
@@ -57,6 +59,26 @@ if (isset($_POST['id_role'])) {
 
 $queryProducts = mysqli_query($config, "SELECT * FROM products ORDER BY P_id DESC");
 $rowProducts = mysqli_fetch_all($queryProducts, MYSQLI_ASSOC);
+
+
+// ini sama kaya yang dibawah tapi lebih simple aja dan ambil dari id yang terbesar jika MIN() => ambil data dari yang terkecil
+$queryNoTrans = mysqli_query($config, "SELECT MAX(id) as id_trans FROM transactions") ;
+$rowNoTrans = mysqli_fetch_assoc($queryNoTrans);
+$id_trans = $rowNoTrans['id_trans'];
+$id_trans++;
+
+// jadi ini ambil data lebih dari 0 tapi musi ditambah ['id_trans] + 1 karena gak mungkin 0 AI soalnya 
+// if(mysqli_num_rows($queryNoTrans) > 0) {
+//   $id_trans = $rowNoTrans['id_trans'] + 1;
+// } else {
+//   $id_trans = 1;
+// }
+
+$format_no = "TR" ;
+$date  = date("dmy");
+$increment_number = sprintf("%03s", $id_trans); //sama aja 
+$no_transaction = $format_no . "-" . $date . "-" . $increment_number; //ini juga sma, diambil dari $increment_number
+// $no_transaction = $format_no . "_" . $date . "_" . str_pad("0", $id_trans, STR_PAD_LEFT) Ini kemungkinan untuk memakai ID unik untuk si transaksi
 ?>
 
 <div class="row">
@@ -105,8 +127,7 @@ $rowProducts = mysqli_fetch_all($queryProducts, MYSQLI_ASSOC);
             <div class="col-sm-4">
               <div class="mb-3">
                 <label for="" class="form-label">Transaction *</label>
-                <input type="text" class="form-control" name="no_transaction"
-                  value="<?= isset($_GET['edit']) ? $rowEdit['user_name'] : '' ?>">
+                <input type="text" class="form-control" value="<?php echo $no_transaction ?>" readonly>
               </div>
               <div class="mb-3">
                 <label for="" class="form-label">Product *</label>
@@ -122,36 +143,29 @@ $rowProducts = mysqli_fetch_all($queryProducts, MYSQLI_ASSOC);
             <div class="col-sm-4">
               <div class="mb-3">
                 <label for="" class="form-label">Cashier Name *</label>
-                <input type="text" class="form-control" name="no_transaction"
-                  value="<?php echo $_SESSION['USER_NAME'] ?>">
+                <input type="text" class="form-control" value="<?php echo $_SESSION['USER_NAME'] ?>">
                 <input type="hidden" class="form-control" value="<?php $_SESSION['ID_USER'] ?>" readonly>
               </div>
             </div>
-            <div class="col-sm-4">
-              <div class="mb-2">
-                <label for="" class="form-label">Transaction *</label>
-                <input type="text" class="form-control" name="no_transaction"
-                  value="<?= isset($_GET['edit']) ? $rowEdit['user_name'] : '' ?>">
-              </div>
-            </div>
           </div>
-
-          <div class="mb-3">
-            <label for="" class="form-label">Email *</label>
-            <input type="email" class="form-control" name="user_email" placeholder="Enter your Email"
-              value="<?= isset($_GET['edit']) ? $rowEdit['user_email'] : '' ?>" required>
+          <div class="mb-3" align="right">
+            <button type="button" class="btn btn-primary" id="inputOrder">AddProduct</button>
           </div>
-
-          <div class="mb-3">
-            <label for="" class="form-label">Password *</label>
-            <input type="password" class="form-control" name="user_password" placeholder="Enter your Password"
-              <?= empty($userId) ? 'required' : ''; ?>>
-            <?php if (isset($_GET['edit'])) : ?>
-            <small>
-              You can change your password by filling the above field
-            </small>
-            <?php endif ?>
-          </div>
+          <table class="table table-bordered" id="productDetail">
+            <thead>
+              <tr>
+                <th>no</th>
+                <th>product</th>
+                <th>Qty</th>
+                <th>price</th>
+                <th>total</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- tambahin javascript -->
+            </tbody>
+          </table>
 
           <div class="mb-3">
             <input type="submit" class="btn btn-success"
@@ -175,23 +189,95 @@ $rowProducts = mysqli_fetch_all($queryProducts, MYSQLI_ASSOC);
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <form action="" method="post">
-        <div class="modal-body">
-          <div class="mb-3">
-            <label for="" class="form-label">Role Name</label>
-            <select name="id_role" id="" class="form-control">
-              <option value="">Select One</option>
-              <?php foreach ($rowRoles as $rowRole): ?>
-              <option value="<?= $rowRole['role_id'] ?>"><?= $rowRole['role_name'] ?></option>
-              <?php endforeach ?>
-            </select>
+        <div class="row">
+          <div class="modal-body">
+            <div class="mb-3">
+              <label for="" class="form-label">Role Name</label>
+              <select name="id_role" id="" class="form-control">
+                <option value="">Select One</option>
+                <?php foreach ($rowRoles as $rowRole): ?>
+                <option value="<?= $rowRole['role_id'] ?>"><?= $rowRole['role_name'] ?></option>
+                <?php endforeach ?>
+              </select>
+            </div>
           </div>
-        </div>
-        <div class="modaldivoter m-3" align="right">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="submit" name="add-user-role" class="btn btn-primary">Save
-            changes</button>
+
+          <div class="modaldivoter m-3" align="right">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" name="add-user-role" class="btn btn-primary">Save
+              changes</button>
+          </div>
         </div>
       </form>
     </div>
   </div>
 </div>
+
+<script>
+const button = document.getElementById("inputOrder");
+const tbody = document.querySelector("#productDetail tbody");
+
+let no = 1;
+button.addEventListener('click', function() {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+  <td>${no}</td>
+  <td><input type='hidden' name='id_product[]' value=''></td>
+  <td><input type='number' name='qty[]' value="0"></td>
+  <td><input type='number' name='qty[]' value="0"></td>
+  <td><input type='hidden' name='total[]'></td>
+  <td>
+  <button class='btn btn-danger btn-sm deleteRow' type='button'>X</button>
+  </td>
+  `;
+  tbody.appendChild(tr);
+  no++;
+});
+tbody.addEventListener('click', function(e) {
+  if (e.target.classList.contains('deleteRow')) {
+    e.target.closest("tr").remove();
+  }
+  updateNumber();
+
+
+});
+
+function updateNumber() {
+  const rows = tbody.querySelectorAll("tr");
+  rows.forEach(function(row, index) {
+    row.cells[0].textContent = index + 1;
+
+  });
+
+  no = rows.length + 1;
+}
+
+// const services =
+
+// document.getElementById('inputOrder').addEventListener('click', function() {
+//   const tbody = document.querySelector('#productDetail tbody');
+//   const row = document.createElement('tr');
+
+//   let serviceOptions = '<option value="">-- Pilih Product --</option>';
+//   services.forEach(service => {
+//     serviceOptions += `<option value="${products.p_id}" data-price="${products.p_price}">
+//             ${products.p_name}
+//         </option>`;
+//   });
+
+//   row.innerHTML = `
+//         <td>
+//             <select name="id_service[]" class="form-control service-select" required>
+//                 ${serviceOptions}
+//             </select>
+//         </td>
+//         <td><input type="number" name="qty[]" class="form-control qty" value="1" min="1"></td>
+//         <td><input type="number" name="harga[]" class="form-control harga" readonly></td>
+//         <td><input type="number" name="total[]" class="form-control total" readonly></td>
+//         <td><button type="button" class="btn btn-danger btn-sm deleteRow">X</button></td>
+//     `;
+
+//   tbody.appendChild(row);
+//   attachEvents(row); // <-- Kirim baris yang baru ditambahkan
+// });
+</script>
